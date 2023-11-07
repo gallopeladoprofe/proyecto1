77ee2d5451b34290dcd71cb37720531be63c1516
gallopelado <juanftp100@gmail.com>
2023-11-06 21:15:18 -0300

    add: integrando adminlte y vistitas

---
+++ static/js_operacional/tabla.js
@@ -0,0 +1,6 @@
+// Señor Javascript, vas a ejecutar código siempre y cuando se haya terminado
+// la carga del HTML
+document.addEventListener('DOMContentLoaded', function() {
+    // zona segura
+    console.log('vista tabla, holis')
+});
