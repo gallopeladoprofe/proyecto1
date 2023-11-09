d4226fa0b5dab013df18148e363ce53747acee2f
gallopelado <juanftp100@gmail.com>
2023-11-08 21:28:35 -0300

    add: usando promesas

--- static/js_operacional/app.js
+++ static/js_operacional/app.js
@@ -1,1 +1,3 @@
-console.log('Holis');
+$(function() {
+    console.log('Holis usando jquery');
+});
--- static/js_operacional/tabla.js
+++ static/js_operacional/tabla.js
@@ -1,6 +1,23 @@
 // Señor Javascript, vas a ejecutar código siempre y cuando se haya terminado
 // la carga del HTML
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function() {
     // zona segura
-    console.log('vista tabla, holis')
+    //console.log('vista tabla, holis')
+    // Uso de promesas
+    const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+    console.log(respuesta.data);
+
+    let la_data = null;
+    la_data = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    console.log(la_data.data);
+    // axios.get('https://jsonplaceholder.typicode.com/posts')
+    //         .then((res) => {
+    //             la_data = res.data;
+    //             console.log(la_data);
+    //         })
+    //         .catch((error) => console.error(error));
+    //console.log(la_data);
+
+    const personas = await axios.get('/hola')
+    console.log(personas.data);
 });
