9e0c5ce4d4c0b308e73934a96d51406593d1e839
gallopelado <juanftp100@gmail.com>
2023-11-27 19:44:18 -0300

    add: agregamos sweet alert y recuperamos elementos del formulario

--- static/js_operacional/referenciales/personas/persona.js
+++ static/js_operacional/referenciales/personas/persona.js
@@ -1,4 +1,27 @@
 $(function() {
     // zona segura de ejecucion
     console.log('Holis, estoy en el formulario persona');
+    
+    
+    $('#btncrear').on('click', function(){
+        // recuperar los datos del formulario
+        const nombres = $('#txtnombres').val();
+        const apellidos = $('#txtapellidos').val();
+        const ci = $('#txtci').val();
+        const direccion = $('#txtdireccion').val();
+        
+        console.log('nombres = ' + nombres);
+        console.log("apellidos = " + apellidos);
+        console.log(`cedula ${ci}`);
+        console.log(`direccion ${direccion}`);
+
+        // JSON -- Javascript object notation
+        const persona = {
+            nombres: nombres,
+            apellidos: apellidos,
+            cedula: ci,
+            direccion: direccion
+        }
+        console.log(persona.nombres);
+    });
 });
